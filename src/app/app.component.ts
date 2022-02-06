805205cff6758929bd8b11dc3dc1f56761b61527
Jasper Kent <software@jasperkent.com>
2022-02-06 11:14:59 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { concat, fromEvent, interval, merge, Observable, of, zip } from 'rxjs';
+import { map, catchError, filter, mergeMap, switchMap, switchAll, mergeAll, concatAll } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'marble-diagrams';
+export class AppComponent implements AfterViewInit {
+  output = '';
+  @ViewChild('fire') fireButton!: ElementRef<HTMLButtonElement>;
+  
+  private observable$!: Observable<any>;
+  private eventObservable$!: Observable<any>;
+
+  ngAfterViewInit(): void {
+    let counter = 0;
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVQWXYZ";
+
+    this.eventObservable$ = fromEvent(this.fireButton.nativeElement, 'click').pipe(
+      map(e => letters[counter++])
+    );  
+  }
+
+  start(): void{
+    this.observable$.pipe(
+    //  catchError(e => `Error: ${e}`)
+    ).subscribe(
+      n => this.output =`${this.output} ${n}`
+    )
+  }
 }
